592958023662d21da3b5d1e5536917aa1dc7eb36
zalvladik <zalipkav@ukr.net>
2023-04-21 20:49:25 +0300

    kk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import PhoneBookContainer from './Counter/Counter'
+import React from 'react'
+
+class App extends React.Component{
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  newState = (name,number) =>{
+    if(JSON.parse(localStorage.getItem('contactsContainer')) === null){
+      return this.firstName(name,number)
+    }
+  
+    if(this.state.contacts.find(option => option.name === `${name}`)){
+      return alert(`${name} is already in contact`)
+    } 
+
+    const id = this.state.contacts.length+1
+
+    const updateSlice = [{id: `id-${id}`, name:`${name}`, number:`${number}`}]
+    const currentState = this.state.contacts
+
+    this.setState({contacts:[...currentState,...updateSlice]})
+  }
+
+  firstName = (name,number) =>{
+    const updateSlice = [{id: `id-${1}`, name:`${name}`, number:`${number}`}]
+    this.setState({contacts:[...updateSlice]})
+  }
+
+  deleteName = (event) =>{
+    const currentState = this.state.contacts
+    const newState = currentState.filter(option => option.id !== `${event.currentTarget.id}`)
+    this.setState({contacts:[...newState]})
+  }
+
+  filterName = (event) =>{
+    this.setState({filter:`${event.currentTarget.value}`})
+  }
+  
+  componentDidUpdate(prevProps, prevState){
+  if(this.state.contacts !== prevState.contacts){
+  localStorage.setItem('contactsContainer', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  componentDidMount(prevProps, prevState){
+    const contactsList = JSON.parse(localStorage.getItem('contactsContainer'))
+  this.setState({contacts:contactsList})
+  }
+
+  render(){
+    
+    const {filter} = this.state
+    const currentState = this.state.contacts
+    const newState = JSON.parse(localStorage.getItem('contactsContainer')) === null 
+    ? false 
+    : currentState.filter(option => option.name.toLowerCase().includes(`${filter.toLowerCase()}`))
+
+    return (
+    <PhoneBookContainer
+    events={newState}
+    deleteName={this.deleteName}
+    filterName={this.filterName}
+    newState={this.newState}
+    />
+    
   );
+}
 };
+
+export default App
---
+++ src/components/Counter/CounterStyled.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+margin: 20px 0px 0px 30px;
+`
+
+export const Form = styled.form`
+border:2px solid black;
+width:300px;
+padding:10px 10px;
+display:flex;
+flex-direction:column;
+border-radius:4px;
+`
+
+export const Title = styled.h2`
+`
+
+export const Text = styled.p`
+margin:0px;
+`
+
+export const Button = styled.button`
+width:100px;
+height:20px;
+border:1px solid black;
+`
+
+export const LiButton =styled.button`
+margin-left:20px;
+width:50px;
+height:20px;
+border:1px solid black;
+`
+
+export const InputText = styled.input`
+width:200px;
+height:20px;
+border:1px solid black;
+margin:10px 0px 20px 0px;
+`
+
+export const UlContacts = styled.ul`
+`
+
+export const LiContacts = styled.li`
+margin-bottom:10px;
+`
