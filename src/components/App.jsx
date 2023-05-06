1a09395a51bdb9b5812ed66738d024d5ddac4335
zalvladik <zalipkav@ukr.net>
2023-05-06 16:45:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,15 +13,15 @@ class App extends Component {
   }
 
   newState = (name,number) =>{
-    if(this.state.contacts !== []){
-      if(this.state.contacts.find(option => option.name.toLowerCase() === `${name}`.toLowerCase())){
-        return alert(`${name} is already in contact`)
+      if(this.state.contacts.length > 0){
+        if(this.state.contacts.find(option => option.name.toLowerCase() === `${name}`.toLowerCase())){
+          return alert(`${name} is already in contact`)
+        }
+    
+        if(this.state.contacts.find(option => option.number === `${number}`)){
+          return alert(`${number} is already in contact`)
+        }
       }
-  
-      if(this.state.contacts.find(option => option.number === `${number}`)){
-        return alert(`${number} is already in contact`)
-      }
-    }
 
     const updateSlice = [{id: `id-${nanoid()}`, name:`${name}`, number:`${number}`}]
     const currentState = this.state.contacts
